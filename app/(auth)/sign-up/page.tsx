356d92e50ad62ea2983c9b308c387a0aab9f3cb6
arifshaikhvk25 <arifshaikhvk25@gmail.com>
2025-03-29 13:53:13 +0530

    [added]:dummy routes / vapi api keys / ignored ts error and eslint error/ implement interview

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -4,7 +4,8 @@ import React from "react";
 const page = () => {
   return (
     <div>
-      sign up <AuthForm type="sign-up" />
+      {/* sign up */}
+      <AuthForm type="sign-up" />
     </div>
   );
 };
